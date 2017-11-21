97947c8f73d3a591b0d4105043c15635daf376b4
EmilioDev <eecardero@uci.cu>
2017-11-20 18:38:32 -0800

    Agregando el tab control

---
+++ CustomControlsLibrary/Viewer/Tabs.xaml.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CustomControlsLibrary.Viewer.ViewModels;
+
+namespace CustomControlsLibrary.Viewer
+{
+    /// <summary>
+    /// Interaction logic for Tabs.xaml
+    /// </summary>
+    public partial class Tabs : UserControl
+    {
+        private List<TabItem> _tabItems;
+        private TabItem _tabAdd;
+        private TabsViewModel _vm;
+        public Tabs()
+        {
+            InitializeComponent();
+            this._vm = new TabsViewModel();
+            try
+            {
+                InitializeComponent();
+
+                // initialize tabItem array
+                _tabItems = new List<TabItem>();
+
+                // add a tabItem with + in header 
+                _tabAdd = new TabItem();
+                _tabAdd.Header = "+";
+                // tabAdd.MouseLeftButtonUp += new MouseButtonEventHandler(tabAdd_MouseLeftButtonUp);
+
+                _tabItems.Add(_tabAdd);
+
+                // add first tab
+                this.AddConfigTabItem();
+
+                // bind tab control
+                tabDynamic.DataContext = _tabItems;
+
+                tabDynamic.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private TabItem AddConfigTabItem()
+        {
+            int count = _tabItems.Count;
+
+            // create new tab item
+            TabItem tab = new TabItem();
+
+            
+            tab.Header = this._vm.Start;
+            tab.Name = "start_tab";
+
+            tab.HeaderTemplate = tabDynamic.FindResource("TabHeader") as DataTemplate;
+
+            tab.MouseDoubleClick += new MouseButtonEventHandler(tab_MouseDoubleClick);
+
+            // add controls to tab item, this case I added just a textbox
+            TextBox txt = new TextBox();
+            txt.Name = "txt";
+
+            tab.Content = txt;
+
+            // insert tab item right before the last (+) tab item
+            _tabItems.Insert(count - 1, tab);
+
+            return tab;
+        }
+
+        private TabItem AddTabItem()
+        {
+            int count = _tabItems.Count;
+
+            // create new tab item
+            TabItem tab = new TabItem();
+
+            tab.Header = string.Format("Tab {0}", count);
+            tab.Name = string.Format("tab{0}", count);
+            tab.HeaderTemplate = tabDynamic.FindResource("TabHeader") as DataTemplate;
+
+            tab.MouseDoubleClick += new MouseButtonEventHandler(tab_MouseDoubleClick);
+
+            // add controls to tab item, this case I added just a textbox
+            TextBox txt = new TextBox();
+            txt.Name = "txt";
+
+            tab.Content = txt;
+
+            // insert tab item right before the last (+) tab item
+            _tabItems.Insert(count - 1, tab);
+
+            return tab;
+        }
+
+        private void tabAdd_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            // clear tab control binding
+            tabDynamic.DataContext = null;
+
+            TabItem tab = this.AddTabItem();
+
+            // bind tab control
+            tabDynamic.DataContext = _tabItems;
+
+            // select newly added tab item
+            tabDynamic.SelectedItem = tab;
+        }
+
+        private void tab_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            TabItem tab = sender as TabItem;
+
+            TabProperty dlg = new TabProperty();
+
+            // get existing header text
+            dlg.txtTitle.Text = tab.Header.ToString();
+
+            if (dlg.ShowDialog() == true)
+            {
+                // change header text
+                tab.Header = dlg.txtTitle.Text.Trim();
+            }
+        }
+
+        private void tabDynamic_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TabItem tab = tabDynamic.SelectedItem as TabItem;
+            if (tab == null) return;
+
+            if (tab.Equals(_tabAdd))
+            {
+                // clear tab control binding
+                tabDynamic.DataContext = null;
+
+                TabItem newTab = this.AddTabItem();
+
+                // bind tab control
+                tabDynamic.DataContext = _tabItems;
+
+                // select newly added tab item
+                tabDynamic.SelectedItem = newTab;
+            }
+            else
+            {
+                // your code here...
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            string tabName = (sender as Button).CommandParameter.ToString();
+
+            var item = tabDynamic.Items.Cast<TabItem>().Where(i => i.Name.Equals(tabName)).SingleOrDefault();
+
+            TabItem tab = item as TabItem;
+
+            if (tab != null)
+            {
+                if((tabDynamic.SelectedItem as TabItem).Name == "start_tab")
+                {
+                    MessageBox.Show("Cannot remove config tab.");
+                }
+                //else if (_tabItems.Count < 3 )
+                //{
+                //    MessageBox.Show("Cannot remove last tab.");
+                //}
+                else if (MessageBox.Show(this._vm.Msg_Delete_Page, "Remove Tab", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    // get selected tab
+                    TabItem selectedTab = tabDynamic.SelectedItem as TabItem;
+
+                    // clear tab control binding
+                    tabDynamic.DataContext = null;
+
+                    _tabItems.Remove(tab);
+
+                    // bind tab control
+                    tabDynamic.DataContext = _tabItems;
+
+                    // select previously selected tab. if that is removed then select first tab
+                    if (selectedTab == null || selectedTab.Equals(tab))
+                    {
+                        selectedTab = _tabItems[0];
+                    }
+                    tabDynamic.SelectedItem = selectedTab;
+                }
+            }
+        }
+    }
+}
---
+++ CustomControlsLibrary/Viewer/ViewModels/TabsViewModel.cs
@@ -0,0 +1,48 @@
+﻿using GlobalDatasLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomControlsLibrary.Viewer.ViewModels
+{
+    internal class TabsViewModel : ViewModel
+    {
+        #region properties
+        public string Start
+        {
+            get
+            {
+                return Current.GetPhraseInCurrentLanguaje("start");
+            }
+        }
+
+        public string Msg_Delete_Page
+        {
+            get
+            {
+                return Current.GetPhraseInCurrentLanguaje("remove_tab");
+            }
+        }
+
+        public string Msg_Error_removing_Config
+        {
+            get
+            {
+                return Current.GetPhraseInCurrentLanguaje("config_removing_error");
+            }
+        }
+        #endregion
+        #region delegates & events
+        public override event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+        #region functions
+        protected override void RaiseProperty(string name)
+        {
+            this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+    }
+}
